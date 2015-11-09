d9dd23cbce3814db1feb1b4ec16449dbdc5a793d
KostiantynG <gorbunovkostiantyn@gmail.com>
2015-11-09 16:24:24 +0200

    SelectionSorter Added

--- Sort Algoritms/src/ua/gko/selection/SelectionSorterTest.java
+++ Sort Algoritms/src/ua/gko/selection/SelectionSorterTest.java
@@ -1,13 +1,13 @@
-package ua.gko.bubble;
+package ua.gko.selection;
 
 import java.util.Arrays;
 
-public class BubbleSorterTest {
+public class SelectionSorterTest {
 	public static void main(String[] args) {
 		int[][] data = { {}, { 1 }, { 0, 2, 1 }, { 2, 3, 1, 0 }, { 0, 2, 1, 3, 5, 4, 6 }, };
 		for (int[] arr : data) {
 			System.out.print(Arrays.toString(arr) + " -> ");
-			BubbleSorter.sort(arr);
+			SelectionSorter.sort(arr);
 			System.out.println(Arrays.toString(arr));
 		}
 	}
